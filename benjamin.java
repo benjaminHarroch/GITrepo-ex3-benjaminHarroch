d0db63387bb32a1c3e90210d04885fdf5c46c43e
benjaminHarroch <bharroch@gmail.com>
2024-05-12 14:38:51 +0300

    i have no more comment to give for this push hahhaa

--- benjamin.java
+++ benjamin.java
@@ -1,7 +1,8 @@
 public class Benjamin{
-	 public static void main(String[] args) {
-	  for (int i=1;i<=10; i++) {
-	   System.out.println("print 10 time the name benjamin");
-	  }
-	 }
+	
+		 public static void main(String[] args) {
+			  for (int i=1;i<=10; i++) {
+			   System.out.println("print 10 time the name benjamin");
+			  }
+		 }
 }
