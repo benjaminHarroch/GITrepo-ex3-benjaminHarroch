6ae245ccf548609e0b51fc5f12f3285273d1e953
benjaminHarroch <bharroch@gmail.com>
2024-05-12 14:32:40 +0300

    fix the type of file and add a build steps and change hello.java and benjamin.java files

--- benjamin.java
+++ benjamin.java
@@ -1,7 +1,7 @@
-public class Hello{
- public static void main(String[] args) {
-  for (int i=1;i<=10; i++) {
-   System.out.println("change the benjamin.java file now fileee");
-  }
- }
+public class Benjamin{
+	 public static void main(String[] args) {
+	  for (int i=1;i<=10; i++) {
+	   System.out.println("print 10 time the name benjamin");
+	  }
+	 }
 }
